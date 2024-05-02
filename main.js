388b8f36e2bb6c76dd4fb931678a3bc34e85cf3c
David Cabrera <109712110+DavidDuca@users.noreply.github.com>
2024-05-02 13:09:38 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let audio = $('#audio');
 
-let response = window.confirm("Are you sure you want to Continue?");
 function $(s) {
     return document.querySelector(s);
   }
 
-if(response == true){
-    audio.play();
-}
+  window.addEventListener("load", (event) => {
+     audio.play();
+    });
